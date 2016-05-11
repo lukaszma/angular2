458162eecc5c10ce29e70248008bc5d677508772
lukaszma <lukasz.a.matyja@gmail.com>
2016-05-11 20:43:48 +0200

    add new project AngularBase

---
+++ a/javascript.js
@@ -0,0 +1,10 @@
+var input = document.querySelector('.input');
+var btn = document.getElementById('btn');
+var p = document.getElementById('text');
+
+
+document.addEventListener("click", function(){
+	var text = input.value;
+	p.innerHTML= text;
+	//alert('Angelisia');
+})
